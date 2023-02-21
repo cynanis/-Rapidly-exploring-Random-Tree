bee2fdd79d8f7d4e0910cf386cafab13dcc44c05
mehrab <anismehrab@gmail.com>
2023-02-21 14:09:21 +0100

    model params

---
+++ cfg.py
@@ -0,0 +1,11 @@
+
+params = {
+                    "L":4.0,
+                    "lr":2.1,
+                    "w_max":1.22,
+                    "delta_max":0.3,
+                    "v_max":4.0,
+                    "sample_time":0.4,
+                    "kp_v":7.0,
+                    "kp_ld":4.0,
+                    "kp_crss":0.8}
