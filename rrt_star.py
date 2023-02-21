5059c942dc993b33796cd383f33858cd62c91dce
mehrab <anismehrab@gmail.com>
2023-02-21 14:55:09 +0100

    add motion model

--- rrt_star.py
+++ rrt_star.py
@@ -8,13 +8,13 @@ from rrt import RRT
 
 
 class RRTStar(RRT):
-    def __init__(self, map, q_star, q_goal, step_size = 10,goal_threshold=8,rewire_radius = 8, obstacles_color= (0,255,0,255)):
+    def __init__(self,map, q_star, q_goal,goal_threshold=8, rewire_radius = 8, obstacles_color= (0,255,0,255)):
         """ 
-            q_star = (x,y)
-            q_goal = (x,y)
+            q_star : starting state {"x":x,"y":y,"theta"=theta,"delta":delta,"beta":beta}
+            q_goal : goal state {"x":x,"y"=y,"theta"=theta,"delta":delta,"beta":beta}
         
         """
-        super().__init__(map, q_star, q_goal, step_size,goal_threshold, obstacles_color)
+        super().__init__(map, q_star, q_goal,goal_threshold, obstacles_color)
         self.tree.add_cost(0)
         self.rewire_radius = rewire_radius
 
@@ -23,54 +23,51 @@ class RRTStar(RRT):
         for i in range(steps):
             q_rand = self.sample_free()
             _,x_nearest = self.nearest(self.tree,q_rand)
-            q_new = self.steer(x_nearest.q,q_rand)
-            if self.is_collision_free(line(x_nearest.q,q_new)): 
+            q_new, trajectory_ = self.steer(x_nearest.q,q_rand)
+            
+            if self.is_collision_free(trajectory_): 
                 # find list of x nodes that lie in the circle centerd at q_new
                 X_near = self.near(self.tree,q_new,self.rewire_radius)     
                 
                  #connect x_new node to the near x node that result in a minimum-cost path
                 x_new = Node(q = q_new)
                 x_min = x_nearest
-                c_min = cost(x_nearest) + c(line(x_nearest.q,q_new))
+                c_min = cost(x_nearest) + ecludian(x_nearest.q,q_new)
                 for x_near in X_near:
-                    if self.is_collision_free(line(x_near.q,q_new)):
-                        if cost(x_near) + c(line(x_near.q,q_new)) < c_min:
+                    if self.is_collision_free(trajectory(x_near.q,q_new)):
+                        if cost(x_near) + ecludian(x_near.q,q_new) < c_min:
                             x_min = x_near
-                            c_min = cost(x_near) + c(line(x_near.q,q_new))
+                            c_min = cost(x_near) + ecludian(x_near.q,q_new)
                 x_min.add_child(x_new)
                 x_new.add_cost(c_min)
 
                 #check if goal reached
-                if is_goal_reached(line(x_min.q,x_new.q),self.goal,self.goal_threshold):
-                    print("goal reached")
+                if is_goal_reached(trajectory(x_min.q,x_new.q),self.goal,self.goal_threshold):
                     goal_reached = True
                 
                 #visulize the updated tree
-                draw_branch(self.map,x_min.q,x_new.q)
+                draw_trajectoy(self.map,trajectory(x_min.q,x_new.q),width=2)
                         
                 #rewrite the tree 
                 for x_near in X_near:
-                    if self.is_collision_free(line(x_new.q,x_near.q)):
-                        if cost(x_new) + c(line(x_new.q,x_near.q)) < cost(x_near):
+                    if self.is_collision_free(trajectory(x_new.q,x_near.q)):
+                        if cost(x_new) + ecludian(x_new.q,x_near.q) < cost(x_near):
                             x_parent = parent(x_near)
                             x_parent.remove_child(x_near)
                             x_new.add_child(x_near)
-                            x_near.add_cost(cost(x_new) + c(line(x_new.q,x_near.q)))
+                            x_near.add_cost(cost(x_new) + ecludian(x_new.q,x_near.q))
                 
                             #check if goal reached
-                            if is_goal_reached(line(x_new.q,x_near.q),self.goal,self.goal_threshold):
-                                print("goal reached")
+                            if is_goal_reached(trajectory(x_new.q,x_near.q),self.goal,self.goal_threshold):
                                 goal_reached = True
 
                             #visulize the updated tree
-                            delete_branch(self.map,x_parent.q,x_near.q)
-                            draw_branch(self.map,x_new.q,x_near.q)
+                            delete_trajectory(self.map,trajectory(x_parent.q,x_near.q),width=2)
+                            draw_trajectoy(self.map,trajectory(x_new.q,x_near.q),width=2)
 
                 if goal_reached:
-                    pygame.draw.circle(self.map,(255,0,0),self.goal,radius =5,width=5)
-                    pygame.display.update()
-                    self.draw_path(x_new,width=3)
-
+                    draw_point(self.map,self.goal,raduis=self.goal_threshold,width=self.goal_threshold,color=(255,0,0))
+                    draw_trajectories_path(self.map,x_new,width=4,color=(255,0,0))
                     return self.tree
 
     
@@ -80,7 +77,7 @@ class RRTStar(RRT):
 
         Args:
             x (Node): root node
-            q (Typle): (x,y) circle center
+            q dict: {"x":x,"y":y} center node
             r (Int): circle raduis
 
         Returns:
@@ -110,12 +107,12 @@ if __name__=="__main__":
     map.fill((255,255,255))
     
     #draw star and goal points
-    q_star=(100,20)
-    q_goal=(400,500)
+    q_start={"x":100,"y":20,"theta":0.3,"delta":0.1,"beta":0.01}
+    q_goal={"x":400,"y":500,"theta":0,"delta":0,"beta":0}
     star_color = (255,0,255,255)
     goal_color = (0,0,255,255)
-    pygame.draw.circle(map,star_color,center=q_star,radius=5,width=5)
-    pygame.draw.circle(map,goal_color,center=q_goal,radius=5,width=5)
+    pygame.draw.circle(map,star_color,center=(q_start["x"],q_start["y"]),radius=5,width=5)
+    pygame.draw.circle(map,goal_color,center=(q_goal["x"],q_goal["y"]),radius=7,width=7)
 
     #draw obstacles
     obstacle_color = (0,255,0,255)
@@ -130,7 +127,7 @@ if __name__=="__main__":
 
 
     # Initializing RTT
-    rrt = RRTStar(map=map,q_star=q_star,q_goal=q_goal,step_size=25,goal_threshold=7, obstacles_color=obstacle_color)
+    rrt = RRTStar(map=map,q_star=q_start,q_goal=q_goal,goal_threshold=7, obstacles_color=obstacle_color)
     #Build RRT
     rrt.build_rrt(int(1e6))
     input('Press ENTER to exit')
