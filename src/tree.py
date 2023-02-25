182e6f8906f67cdbbac1d2f04bc473695746f1b6
mehrab <anismehrab@gmail.com>
2023-02-25 19:27:35 +0100

    add weight parameter to the tree

--- src/tree.py
+++ src/tree.py
@@ -1,21 +1,19 @@
 class Node:
 
-    def __init__(self,q,u=None):
+    def __init__(self,q,u=None,w=None):
         """ q = {"x":x,"y"=y,"theta"=theta,"delta":delta,"beta":beta} """
         self.q = q
         self.u = u
+        self.w = w
         self.children = []
-        self.cost = None
         self.parent = None
-        self.goal_reached = False
 
     def add_child(self,child_x):
         child_x.parent = self
         self.children.append(child_x)
     
     def remove_child(self,child_x):
-        self.children = [child for child in self.children if child is not child_x]
+        self.children = [child for child in self.children if child is not child_x] 
     
-    def add_cost(self,cost):
-        self.cost = cost    
-        
+    def add_weight(self,w):
+        self.w = w    
