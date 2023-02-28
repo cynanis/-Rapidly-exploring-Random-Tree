88d2d75821baf1e1d2fdcbd2ab85c5a45a82d582
mehrab <anismehrab@gmail.com>
2023-02-28 12:24:01 +0100

    reduce trajectory length to 2steps

--- src/cfg.py
+++ src/cfg.py
@@ -4,8 +4,9 @@ params = {
                     "lr":2.1,
                     "w_max":1.22,
                     "steer_max":0.4,
-                    "v_max":4.0,
-                    "sample_time":0.4,
-                    "kp_v":7.0,
+                    "v_max":40.0,
+                    "sample_time":0.1,
+                    "kp_v":1.0,
                     "kp_ld":4.0,
-                    "kp_crss":0.8}
+                    "kp_crss":0.8,
+                    "step_samples":2}
